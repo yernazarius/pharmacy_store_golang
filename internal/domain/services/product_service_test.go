b99e9b4d3be20a4df3f3362104e9ff77561fea4e
yernazarius <ernzaryakupov04@gmail.com>
2024-06-05 00:48:53 +0500

    update

--- internal/domain/services/product_service_test.go
+++ internal/domain/services/product_service_test.go
@@ -3,16 +3,20 @@ package services
 import (
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/yernazarius/pharmacy_store_golang/internal/domain/entities"
-	"github.com/yernazarius/pharmacy_store_golang/mocks"
+	"github.com/yernazarius/pharmacy_store_golang/internal/mocks"
 )
 
 func TestGetProducts(t *testing.T) {
-	mockRepo := &mocks.ProductRepository{}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mocks.NewMockProductRepository(ctrl)
 	service := &ProductService{Repo: mockRepo}
 
-	mockRepo.On("GetProducts").Return([]entities.Product{
+	mockRepo.EXPECT().GetProducts().Return([]entities.Product{
 		{ID: 1, Name: "Product 1", Price: 10.0},
 		{ID: 2, Name: "Product 2", Price: 20.0},
 	}, nil)
